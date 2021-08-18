60e6d49f4b7abb11f1b0b437b888aecef58aa49e
Urbansan <urbansanek@gmail.com>
2021-08-18 16:42:57 +0200

    fixed issues with finding subpackages

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 import setuptools
 
 
-with open("long_description.md", "r") as f:
+with open("README.md", "r") as f:
     long_description = f.read()
+
 
 setuptools.setup(
     name="shell_multiprocess",
-    descrption="Simple shell script which runs other shell commands multi   in parallel",
-    version="1.0.0",
-    packages=["shell_multiprocess"],
+    description="Simple shell script which runs lists of shell commands in parallel",
+    version="1.0.2",
+    author="Kris Urbanczyk",
+    author_email="urbansanek@gmail.com",
+    project_urls={
+        "Source Code": "https://github.com/urbansan/futures_subproc",
+    },
+    packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["psutil", "dataclasses; python_version < '3.7'", "aiofiles"],
     entry_points={
